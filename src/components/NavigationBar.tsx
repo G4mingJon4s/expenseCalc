6877a650badafe257e88e885ed6db5bde75c8924
G4mingJon4s <Jthelker@gmail.com>
2023-01-31 20:46:49 +0100

    fixed nav bar button hover, fixed page redirects

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -13,9 +13,9 @@ export default function NavigationBar({ currentMonth, monthIsNegative, toggleMon
 		<nav className={styles["container"]}>
 			<Month currentMonth={currentMonth} toggleMonthModal={toggleMonthModal} isNegative={monthIsNegative}/>
 			<ul className={styles["links"]}>
-				<li className={styles["item"]}><a href="" className={styles["link"]}>Overview</a></li>
-				<li className={styles["item"]}><a href="#expenses" className={styles["link"]}>Expenses</a></li>
-				<li className={styles["item"]}><a href="#subscriptions" className={styles["link"]}>Subscriptions</a></li>
+				<li className={styles["item"]}><a href="/" className={styles["link"]}>Overview</a></li>
+				<li className={styles["item"]}><a href="/expenses" className={styles["link"]}>Expenses</a></li>
+				<li className={styles["item"]}><a href="/income" className={styles["link"]}>Income</a></li>
 			</ul>
 		</nav>
 	);
@@ -29,11 +29,9 @@ interface MonthProps {
 
 export function Month({ currentMonth, isNegative, toggleMonthModal }: MonthProps) {
 	return (
-		<div className={styles["month-container"]}>
-			<button className={`${styles["change-month"]} ${styles[isNegative ? "red" : "green"]}`} onClick={toggleMonthModal}>
-				<HiCalendar size={24}/>
-				<span className={styles["month-text"]}>{currentMonth}</span>
-			</button>
-		</div>
+		<button className={`${styles["month-container"]} ${styles["change-month"]} ${styles[isNegative ? "red" : "green"]}`} onClick={toggleMonthModal}>
+			<HiCalendar size={24}/>
+			<span className={styles["month-text"]}>{currentMonth}</span>
+		</button>
 	);
 }
